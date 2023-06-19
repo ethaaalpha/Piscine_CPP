f51670d82e1b2dc3039c17eaef4e02eb9a323f1f
ethaaalpha <geekofgamer@gmail.com>
2023-06-19 23:31:56 +0200

    megaphone trimmed

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+void ft_trim(std::string item, char *dest)
+{
+	size_t start = item.find_first_not_of(" ");
+	size_t end = item.find_last_not_of(" ");
+	if (start == std::string::npos)
+		dest[0] = '\0';
+	else
+	{
+		item.copy(dest, end - start + 1, start);
+		dest[end - start + 1] = '\0';
+	}
+}
+
 int main(int argc, char **argv)
 {
 	int	i = 1;
@@ -10,11 +23,22 @@ int main(int argc, char **argv)
 	{
 		while (i < argc)
 		{
+			ft_trim(argv[i], argv[i]);
+			i++;
+		}
+		i = 1;
+		while (i < argc)
+		{
 			int	j = 0;
-			while (argv[i][j++])
+			ft_trim(argv[i], argv[i]);
+			while (argv[i][j])
 			{
-str
+				std::cout << (char) toupper(argv[i][j]);
+				j++;
 			}
+			if (argv[i][0] != '\0' && argv[i + 1] && argv[i + 1][0])
+				std::cout << " ";
+			i++;
 		}
 		std::cout << std::endl;
 	}
