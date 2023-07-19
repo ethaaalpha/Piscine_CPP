a1908dbceca5b3c7c83a8b0a2285a6c5c431e6c5
Ethaniel Billon <ebillon@z1r3p5.42lyon.fr>
2023-07-19 12:34:27 +0200

    update and fix for CPP 01

--- CPP01/ex01/zombieHorde.cpp
+++ CPP01/ex01/zombieHorde.cpp
@@ -3,10 +3,11 @@
 
 Zombie* zombieHorde(int N, std::string name)
 {
-	Zombie* horde = new Zombie[N];
-
+	if (N <= 0)
+		return (NULL);
+		
+	Zombie* horde = new Zombie[N];	
 	for (int i = 0; i < N; i++)
 		horde[i].setName(name);
-	
 	return (horde);
 }
--- CPP03/ex00/main.cpp
+++ CPP03/ex00/main.cpp
@@ -3,18 +3,24 @@
 int main(void)
 {
 	ClapTrap ct = ClapTrap("Jesous");
-	ct.attack("alfred");
-	ct.attack("alfred");
-	ct.attack("alfred");
-	ct.attack("alfred");
-	ct.attack("alfred");
-	ct.attack("alfred");
-	ct.attack("alfred");
-	ct.attack("alfred");
-	ct.attack("alfred");
-	ct.attack("alfred");
-	ct.attack("alfred");
-	ct.attack("alfred");
+	ct.attack("Alfred");
+	ct.attack("Louis");
+	ct.attack("Jean");
+	ct.attack("Jacques");
+	ct.attack("Timothe");
+
+	ct.takeDamage(9);
+
+	ct.beRepaired(1);
+	ct.beRepaired(1);
+	ct.beRepaired(1);
+	ct.beRepaired(1);
+	ct.beRepaired(1);
+	ct.beRepaired(1);
+
+	ct.takeDamage(10);
+	ct.beRepaired(1);
+	ct.attack("Alfred");
 	
 	return 0;
 }
