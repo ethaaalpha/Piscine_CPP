e5b6591d5321e440e60154c0c2d398f0e7f4f1f3
ethaaalpha <geekofgamer@gmail.com>
2023-09-04 14:28:26 +0200

    update need more test for the main

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -5,17 +5,53 @@
 template <typename T>
 class MutantStack : public std::stack<T>
 {
-public: 
+public:
+	
+	typedef typename std::stack<T>::container_type::iterator 				iterator;
+	typedef typename std::stack<T>::container_type::const_iterator 			const_iterator;
+	typedef typename std::stack<T>::container_type::reverse_iterator 		reverse_iterator;
+	typedef typename std::stack<T>::container_type::const_reverse_iterator 	const_reverse_iterator;
+
 	MutantStack(void);
 	MutantStack(const MutantStack& parent) : std::stack<T>(parent) {}
 	MutantStack& operator=(const MutantStack& parent)
 	{
-		std::stack<T>::c.begin();
-		https://stackoverflow.com/questions/525365/does-stdstack-expose-iterators
 		std::stack<T>::operator=(parent);
 		return (*this);
 	}
 	~MutantStack(void);
+
+	iterator begin(void) { 
+		return (std::stack<T>::c.begin()); 
+	}
+
+	const_iterator cbegin(void) const {
+		return (std::stack<T>::c.begin());
+	}
+
+	iterator end(void) {
+		return (std::stack<T>::c.end());
+	}
+
+	const_iterator end(void) const {
+		return (std::stack<T>::c.end());
+	}
+
+	reverse_iterator rbegin (void) {
+		return (std::stack<T>::c.rbegin());
+	}
+	
+	const_reverse_iterator rbegin (void) const {
+		return (std::stack<T>::c.rbegin());
+	}
+
+	reverse_iterator rend (void) {
+		return (std::stack<T>::c.rend());
+	}
+	
+	const_reverse_iterator rend (void) const {
+		return (std::stack<T>::c.rend());
+	}
 
 };
 
