f04e674b497212b67f57ff0d530bc3748a877676
ethaaalpha <geekofgamer@gmail.com>
2023-09-15 12:43:21 +0200

    update

--- CPP09/ex01/RPN.cpp
+++ CPP09/ex01/RPN.cpp
@@ -20,9 +20,18 @@ void	RPN::doCalculation(std::string n_list)
 {
 	try {
 		checkStringSyntax(n_list);
+		doRPN();
 	} catch (std::exception &e) {
 		std::cout << "Error : " << e.what() << std::endl;
 	}
+}
+
+void	RPN::checkEachItems(std::string str)
+{
+	if (str.find_first_of("+-/*") != std::string::npos && str.length() != 1)
+		throw (InvalidCharacterException());
+	if (str.length() > 2 || std::atoi(str.c_str()) > 10)
+		throw (HighNumberException());
 }
 
 void	RPN::checkStringSyntax(std::string str)
@@ -34,15 +43,80 @@ void	RPN::checkStringSyntax(std::string str)
 	while (str.find(' ', i) != std::string::npos)
 	{
 		pos = str.find(' ', i);
-		_stack.push(str.substr(i, pos));
+		checkEachItems(str.substr(i, pos - i));
+		_stack.push(str.substr(i, pos - i));
 		i = pos + 1;
 	}
-	std::cout << _stack.top() << std::endl;
-	_stack.pop();
-	std::cout << _stack.top() << std::endl;
-	_stack.pop();
-	std::cout << _stack.top() << std::endl;
-	_stack.pop();
-	std::cout << _stack.top() << std::endl;
-	_stack.pop();
+	checkEachItems(str.substr(i, pos - str.size()));
+	_stack.push(str.substr(i, pos - str.size()));
+	
+	std::stack<std::string> temp;
+	while (_stack.size() > 0)
+	{
+		temp.push(_stack.top());
+		_stack.pop();
+	}
+	_stack = temp;
+}
+
+int RPN::isNumber(std::string str)
+{
+	if (str.find_first_not_of("+-/*") != std::string::npos)
+		return (1);
+	return (0);
+}
+
+std::string RPN::itos(int value)
+{
+	std::stringstream ss;
+
+	ss << value;
+	return (ss.str());
+}
+
+int		RPN::doOperation(int left, int right, std::string sign)
+{
+	if (sign == "-") {
+		return (left - right);
+	} else if (sign == "+") {
+		return (left + right);
+	} else if (sign == "*") {
+		return (left * right);
+	} else {
+		if (right == 0)
+			throw (IllegalActionException());
+		return (left / right);
+	}
+}
+
+void	RPN::doRPN(void)
+{
+	std::stack<std::string> memory;
+	int						right;
+	int						left;
+
+	if (_stack.size() < 3)
+		throw (InvalidCalculationSyntaxeException());
+	while (_stack.size() > 0)
+	{
+		if (!isNumber(_stack.top()) && memory.size() < 2)
+			throw (InvalidCalculationSyntaxeException());
+		else if (isNumber(_stack.top()))
+		{
+			memory.push(_stack.top());
+			_stack.pop();
+		}
+		else if (!isNumber(_stack.top()))
+		{
+			right = std::atoi(memory.top().c_str());
+			memory.pop();
+			left = std::atoi(memory.top().c_str());
+			memory.pop();
+			memory.push(itos(doOperation(left, right, _stack.top())));
+			_stack.pop();
+		}
+	}
+	if (memory.size() != 1)
+		throw (InvalidCalculationSyntaxeException());
+	std::cout << std::atoi(memory.top().c_str()) << std::endl;
 }
--- CPP09/ex01/RPN.hpp
+++ CPP09/ex01/RPN.hpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stack>
 #include <exception>
+#include <sstream>
 
 class RPN
 {
@@ -12,12 +13,35 @@ public:
 	RPN& operator=(const RPN& parent);
 	~RPN(void);
 
-	void	doCalculation(std::string n_list);
-	void	checkStringSyntax(std::string string);
+	void		doCalculation(std::string n_list);
+	void		checkStringSyntax(std::string string);
+	void		checkEachItems(std::string str);
+	void		doRPN(void);
+	int			doOperation(int left, int right, std::string sign);
+
+
+	int 		isNumber(std::string str);
+	std::string itos(int value);
+
 
 
 	class InvalidCharacterException : public std::exception {
 		public	:
 			virtual const char * what() const throw() { return ("invalid character found !");}
+	};
+
+	class HighNumberException : public std::exception {
+		public :
+			virtual const char * what() const throw() { return ("number too high, should be between 0 and 10 !");}
+	};
+
+	class InvalidCalculationSyntaxeException : public std::exception {
+		public :
+			virtual const char * what() const throw() { return ("syntax of numbers and operator is incorrect !");}
+	};
+
+	class IllegalActionException : public std::exception {
+		public :
+			virtual const char * what() const throw() { return ("i think you tried to do something illegal !");}
 	};
 };
