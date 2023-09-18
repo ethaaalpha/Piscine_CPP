dad94de73d038d0c9665b9bb0a3af6d7a342c0fa
Ethaniel Billon <ebillon@z2r4p6.42lyon.fr>
2023-09-18 16:12:13 +0200

    update errors strings

--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -32,49 +32,49 @@ public:
 	class FileErrorException : public std::exception
 	{
 	public:
-		virtual const char * what() const throw () { return ("An error occured with the input file !");}
+		virtual const char * what() const throw () { return ("can't handle the input file !");}
 	};
 
 	class FileEmptyException : public std::exception
 	{
 	public:
-		virtual const char * what() const throw () { return ("The file is empty or do not contain any value !");}
+		virtual const char * what() const throw () { return ("the file is empty or do not contain any value !");}
 	};
 
 	class LineTooShortException : public std::exception
 	{
 	public:
-		virtual const char * what() const throw () { return ("Line is too short !");}
+		virtual const char * what() const throw () { return ("line is too short !");}
 	};
 
 	class InvalidCharacterException : public std::exception
 	{
 	public:
-		virtual const char * what() const throw () { return ("Invalid character found !");}
+		virtual const char * what() const throw () { return ("invalid character found !");}
 	};
 
 	class LineSyntaxException : public std::exception
 	{
 	public:
-		virtual const char * what() const throw () { return ("Invalid line syntax ! (year-mm-dd | value)");}
+		virtual const char * what() const throw () { return ("invalid line syntax ! (year-mm-dd | value)");}
 	};
 
 	class EarlyDateException : public std::exception
 	{
 	public:
-		virtual const char * what() const throw () { return ("Date can't be inferior of the bitcoin creation ! (2009-01-02)");}
+		virtual const char * what() const throw () { return ("date can't be inferior of the bitcoin creation ! (2009-01-02)");}
 	};
 
 	class InvalidCalendarException : public std::exception
 	{
 	public:
-		virtual const char * what() const throw () { return ("Oups I think you missed something because there is only 12 months, and maximum 31 days in a month !");}
+		virtual const char * what() const throw () { return ("oups I think you missed something because there is only 12 months, and maximum 31 days in a month !");}
 	};
 
 	class InvalidNumberException : public std::exception
 	{
 	public:
-		virtual const char * what() const throw () { return ("The value might be a positive float or int, between 0 and 1000 !");}
+		virtual const char * what() const throw () { return ("the value might be a positive float or int, between 0 and 1000 !");}
 	};
 };
 
